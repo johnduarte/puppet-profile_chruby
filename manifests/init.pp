a2a01e18b964840877790f6eac1cc236f2e3bb00
John Duarte <john.duarte@puppetlabs.com>
2016-01-20 16:11:16 -0800

    (QA-2095) Source ~/.profile fix
    This commit fixes a bug introduced at `28c727c`.
    Previously, the intent was to add a source line for `.profile`
    if present. However, `.bashrc` was sourced instead. This
    commit corrects that.

--- manifests/init.pp
+++ manifests/init.pp
@@ -150,7 +150,7 @@ class profile_chruby (
   # ensure that .profile and .bashrc are sourced
   # .profile should be first
   # .bashrc should be last
-  exec {  "grep -q -F '.profile' ${home}/.bash_profile || echo 'if [ -f \"\${HOME}/.profile\" ] ; then\n  source \"\${HOME}/.bashrc\"\nfi' > /tmp/bash_profile && cat ${home}/.bash_profile >> /tmp/bash_profile && mv /tmp/bash_profile ${home}/.bash_profile":
+  exec {  "grep -q -F '.profile' ${home}/.bash_profile || echo 'if [ -f \"\${HOME}/.profile\" ] ; then\n  source \"\${HOME}/.profile\"\nfi' > /tmp/bash_profile && cat ${home}/.bash_profile >> /tmp/bash_profile && mv /tmp/bash_profile ${home}/.bash_profile":
     require => File["${home}/.bash_profile"],
   }
   ->
