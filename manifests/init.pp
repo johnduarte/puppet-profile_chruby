91af562fc633334d19f93afbadfed0220c4a879a
John Duarte <john.duarte@puppetlabs.com>
2016-03-09 17:36:22 -0800

    (QA-2404) Manage chruby at system level
    This commit removes the username parameter and all resources
    previously managed at the user level. The calls to chruby
    are now inserted into the system level bashrc file. Therefore
    the specified ruby version will be the default ruby version
    for all users in a bash non-login shell.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,67 +1,27 @@
 #
 class profile_chruby (
-  $username = 'puppet',
   $ruby_ver = '2.1.6'
 ) {
 
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
-  Package {
-    provider => $::osfamily ? {
-      Darwin  => 'brew',
-      default => undef
-    }
-  }
   case $::osfamily {
     'Darwin': {
-      $home = "/Users/${username}"
-      $managehome = false
       $chruby_group = 'wheel'
-      $group = 'staff'
-      $sudo_group = 'wheel'
-      warning('sudo package installation not supported on OS X')
-      file { $home:
-        ensure  => directory,
-        owner   => $username,
-        group   => $group,
-        require => User[$username],
-      }
+      $global_bashrc = '/etc/bashrc'
+      $pkg_provider = 'brew'
+      contain homebrew
     }
     'Debian': {
-      $home = "/home/${username}"
-      $managehome = true
       $chruby_group = 'root'
-      $group = $username
-      $sudo_group = 'sudo'
-      package { 'sudo':
-        ensure => present,
-        before => User[$username],
-      }
+      $global_bashrc = '/etc/bash.bashrc'
+      $pkg_provider = undef
     }
     default: {
-      $home = "/home/${username}"
-      $managehome = true
       $chruby_group = 'root'
-      $group = $username
-      $sudo_group = 'wheel'
-      package { 'sudo':
-        ensure => present,
-        before => User[$username],
-      }
+      $global_bashrc = '/etc/bashrc'
+      $pkg_provider = undef
     }
-  }
-
-  # FIXME: Find out why OS X does not use profile.d
-  file { '/etc/profile.d':
-    ensure => directory,
-    before => Class['chruby'],
-  }
-
-  user { $username:
-    ensure     => present,
-    managehome => $managehome,
-    shell      => '/bin/bash',
-    groups     => [$sudo_group],
   }
 
   class { 'ruby_build':
@@ -74,51 +34,19 @@ class profile_chruby (
     group   => $chruby_group,
   }
 
-  file{ "${home}/.bashrc":
-    ensure  => present,
-    owner   => $username,
-    group   => $group,
-    require => User[$username],
-  }
-
-  file_line{'Add chruby to .bashrc':
-    path    => "${home}/.bashrc",
+  file_line{'Add chruby to global bashrc':
+    path    => "${global_bashrc}",
     line    => 'source /usr/local/share/chruby/chruby.sh',
-    require => File["${home}/.bashrc"],
-  }
-  file_line{'Add chruby auto to .bashrc':
-    path    => "${home}/.bashrc",
+  }->
+  file_line{'Add chruby auto to global bashrc':
+    path    => "${global_bashrc}",
     line    => 'source /usr/local/share/chruby/auto.sh',
-    require => File["${home}/.bashrc"],
-  }
-
-  file{"${home}/.ruby-version":
-    ensure  => present,
-    content => $ruby_ver,
-    owner   => $username,
-    group   => $group,
-    require => User[$username],
-  }
-
-  file{"${home}/.bash_profile":
-    ensure  => present,
-    owner   => $username,
-    group   => $group,
-    require => User[$username],
-  }
-
-  # ensure that .profile and .bashrc are sourced
-  # .profile should be first
-  # .bashrc should be last
-  exec {  "grep -q -F '.profile' ${home}/.bash_profile || echo 'if [ -f \"\${HOME}/.profile\" ] ; then\n  source \"\${HOME}/.profile\"\nfi' > /tmp/bash_profile && cat ${home}/.bash_profile >> /tmp/bash_profile && mv /tmp/bash_profile ${home}/.bash_profile":
-    require => File["${home}/.bash_profile"],
-  }
-  ->
-  exec {  "grep -q -F '.bashrc' ${home}/.bash_profile || echo 'if [ -f \"\${HOME}/.bashrc\" ] ; then\n  source \"\${HOME}/.bashrc\"\nfi' >> ${home}/.bash_profile":
-    require => File["${home}/.bash_profile"],
+  }->
+  file_line{'Add chruby ruby version to global bashrc':
+    path    => "${global_bashrc}",
+    line    => "chruby ${ruby_ver}",
   }
 
   ruby_build::install_ruby { $ruby_ver: }
-
 
 }
